0d0264c48721ab08ff38c02e1f48f6310c467d2e
Momohson <Remimomoh5@gmail.com>
2022-09-09 06:33:13 -0700

    fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  *Return: By defaults zero if no errors otherwise returns non-zero value.
  */
 
-int main (void)
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
