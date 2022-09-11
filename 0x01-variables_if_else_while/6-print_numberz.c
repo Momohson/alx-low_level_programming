8bfd78463832347ea4947af6f5627e543d51ae1f
Momohson <Remimomoh5@gmail.com>
2022-09-10 18:06:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		puchar(i);
+	}
+	puchar('\n');
+	return (0);
+}
