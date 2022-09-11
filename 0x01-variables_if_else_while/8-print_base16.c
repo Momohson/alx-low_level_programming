7010eb19a0b0e3b8de6a7c18ae1d50e893ffa01f
Momohson <Remimomoh5@gmail.com>
2022-09-10 18:22:52 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+	putchra('\n');
+	return (0);
+}
