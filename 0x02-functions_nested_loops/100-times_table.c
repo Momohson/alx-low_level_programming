898fe0d5d21e2946ba4803946442a15e909e550c
Momohson <Remimomoh5@gmail.com>
2022-09-13 19:33:38 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 			if (z > 99)
 			{
 				_putchar(z / 100 + '0');
-				_putcahr((z / 10 % 10) + '0');
+				_putchar((z / 10 % 10) + '0');
 				_putchar(z % 10 + '0');
 			}
 			else if (z > 9)
