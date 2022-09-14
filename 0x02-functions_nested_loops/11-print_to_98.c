78345ca2c16a26aee66d6a607dbb3249eb9ab03f
Momohson <Remimomoh5@gmail.com>
2022-09-13 19:16:19 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 			{
 				printf(", ");
 			}
-			n++;
+			n--;
 		}
 	}
 	else
@@ -28,7 +28,7 @@ void print_to_98(int n)
 			{
 				printf(",");
 			}
-			n--;
+			n++;
 		}
 	}
 	printf("\n");
