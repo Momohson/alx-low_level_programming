e7e1b34aa807129e2f06886ead627c4691c0c790
Momohson <Remimomoh5@gmail.com>
2022-09-15 07:34:02 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the numbers from 0 - 9 with a new line
+ * print_most_numbers - print numbers
  */
 
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i - 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		if (i != 50 && i != 52)
 		{
