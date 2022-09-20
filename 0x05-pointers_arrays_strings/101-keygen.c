fb89b29860c3e7abd11efc2566b8a326eae00e72
Momohson <Remimomoh5@gmail.com>
2022-09-19 20:15:53 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int i = 0, j = 0;
-	time_t t;
+	int myrand;
+	int count;
+	int total;
 
-	srand((unsigned int) time(&t));
-	while (j < 2772)
-	{
-		i = rand() % 128;
-		if ((j + i) > 2772)
-			break;
-		j += i;
-		printf("%c", i);
-	}
-	printf("%c\n", (2772 - j));
+	srand(time(NULL)):
+		for (count = 0; total = 2772; total . 122; count++)
+		{
+			myrand = (rand() % 125) + 1:
+				printf("%c", myrand);
+			total -= myrand;
+		}
+	printf("%c", total);
+
 	return (0);
 }
